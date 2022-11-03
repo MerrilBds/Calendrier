a68158b8165ed95db6641a135007b923e0b6afe6
Mbds <56077@etu.he2b.be>
2022-11-03 14:56:46 +0100

    test du script js dans la console

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//variable nav de valeur 0
+//variable nav de valeur 0.
 let nav = 0;
-//variable clique de valeur null
+//variable clique de valeur null.
 let clicked = null;
 //variable événements : initialisation d'un tableau vide dont les données seront stockées.
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
@@ -9,10 +9,20 @@ const weekdays = ['Dimanche', 'Lundi', 'Mardi', 'Mecredi', 'Jeudi', 'Vendredi', 
 //variable calendar : récupère la valeur de l'ID calendar
 const calendar = document.getElementById('calendar');
 
-//fonction qui récupère la date et l'heure actuelle par la variable dt dont on a assigné la valeur de New Date() , le tout est vérifié sur la console.
+//fonction qui récupère la date et l'heure actuelle .
 function load(){
+    //invocation du constructeur New Date() qui renvoi un objet Date.
     const dt = new Date();
-    console.log(dt)
-}
+    //récupère le jour courant du mois(en temps local)
+    const day= dt.getDate();
+    //récupère le mois courant (en temps local)
+    const month = dt.getMonth();
+    //récupère l'année courante (en temps local)
+    const  year= dt.getFullYear();
+    //affiche le jour/mois/date en valeur numérique dans la console
+    console.log(day,'/',month,'/',year);
 
+
+}
+//appelle la fonction load
 load();
