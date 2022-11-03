ef793c28858377ba7ff24f10f21789a9fb0a3706
Mbds <56077@etu.he2b.be>
2022-11-03 21:44:00 +0100

    ajout bouton next + back et Ajout d'une boucle

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let clicked = null;
 //variable événements : initialisation d'un tableau vide dont les données seront stockées.
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 //variable weekdays : initialisation de la variable semaineJour regroupant un tableau de caractères représentant les jour de la semaines
-const weekdays = ['Dimanche', 'Lundi', 'Mardi', 'Mecredi', 'Jeudi', 'Vendredi', 'Samedi'];
+const weekdays = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
 //variable calendar : récupère la valeur de l'ID calendar
 const calendar = document.getElementById('calendar');
 
@@ -24,13 +24,34 @@ function load(){
     //invocation du constucteur New Date() qui renvoi le dernier jour   du mois de l'année courante.
     const daysInMonth = new Date(year,month + 1, 0).getDate();
     //affiche le premier jour du mois de l'année courante
-    const dateString = firstDayOfMonth.toLocaleDateString('fr-FR', {
+    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
         weekday: 'long',
         year: 'numeric',
         month: 'numeric',
         day: 'numeric',
       });
-console.log(dateString);
-}
+    // variable paddingDays qui retourne le nombre de jour dans une semaine sans compter les weekend
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+    
+    document.getElementById('monthDisplay').innerText = 
+    `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+
+  calendar.innerHTML = '';
+
+     
+    for(let i =1; i <= paddingDays + daysInMonth; i++){
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        if(i > paddingDays){
+            daySquare.innerText = i - paddingDays;
+
+            daySquare.addEventListener('click', () => console.log('click'));
+        }else{
+            daySquare.classList.add('padding');
+        }
+          calendar.appendChild(daySquare);
+        }
+    }
 //appelle la fonction load
 load();
